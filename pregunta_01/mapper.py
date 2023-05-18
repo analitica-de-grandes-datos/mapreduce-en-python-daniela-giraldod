f0e477d4614d8b8122c7bb8e0708c0819b0dc287
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-17 23:02:17 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        creditHistory = line.split(',')
+        word =  creditHistory[2]
+        sys.stdout.write("{}\t1\n".format(word))
