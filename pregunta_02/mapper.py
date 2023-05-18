702bc4ec6a2be55f6c7cb6ff1e2f649ffcb06215
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-17 23:07:53 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    fields = line.split(',')
+    print('%s, %s' % (fields[3], fields[4]))
