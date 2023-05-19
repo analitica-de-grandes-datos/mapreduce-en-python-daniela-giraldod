55f48d998d5869fd264c97f8d6727edc96c5a09c
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 17:42:29 -0500

    Update mapper.py

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        fila = line.split()
+        sys.stdout.write("{}\t{}\n".format(fila[0], fila[2]))
