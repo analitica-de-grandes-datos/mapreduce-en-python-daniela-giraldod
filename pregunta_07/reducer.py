506df793196b3df0c8e83d6f3c9c0ec92b5bc624
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 17:47:24 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+if __name__ == '__main__':
+
+    word = []
+    num = []
+
+    for line in sys.stdin:
+        word.append(line.split("\t")[0])
+        num.append(int(line.split("\t")[2]))
+
+    ky_value = zip(word, num)
+
+    ky_value_2 = sorted(ky_value,key=lambda x:(x[0],x[1]))
+
+    for word, date, num in ky_value_2:
+      sys.stdout.write("{}   {}   {}\n".format(word, num))
