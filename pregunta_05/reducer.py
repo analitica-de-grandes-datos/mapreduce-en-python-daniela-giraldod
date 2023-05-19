8c847c2b4543e7dfc07f6f0228f73f6a42a5e313
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 16:59:21 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -6,18 +6,18 @@
 import sys
 
 if __name__ == '__main__':
-curkey = None
-total = 0
+    curkey = None
+    total = 0
 
-for line in sys.stdin:
-    key, val = line.split("\t")
-    val = int(val)
-    if key == curkey:
-     total += val
-    else:
-      if curkey is not None:
-        sys.stdout.write("{}\t{}\n".format(curkey, total))
-        curkey = key
-        total = val
-sys.stdout.write("{}\t{}\n".format(curkey, total))
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        if key == curkey:
+         total += val
+        else:
+          if curkey is not None:
+            sys.stdout.write("{}\t{}\n".format(curkey, total))
+            curkey = key
+            total = val
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
 
