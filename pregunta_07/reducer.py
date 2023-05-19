e204cbdc085595f1ba58b5b77eef43950d02597b
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 18:20:17 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -6,15 +6,17 @@ import sys
 if __name__ == '__main__':
 
     word = []
+    date = []
     num = []
 
     for line in sys.stdin:
         word.append(line.split("\t")[0])
+        date.append(line.split("\t")[1])
         num.append(int(line.split("\t")[2]))
 
-    ky_value = zip(word, num)
+    ky_value = zip(word,date, num)
 
-    ky_value_2 = sorted(ky_value,key=lambda x:(x[0],x[1]))
+    ky_value_2 = sorted(ky_value,key=lambda x:(x[0],x[2]))
 
     for word, date, num in ky_value_2:
-      sys.stdout.write("{}   {}   {}\n".format(word, num))
+      sys.stdout.write("{}   {}   {}\n".format(word,date, num))
