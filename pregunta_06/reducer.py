1f7efec8d1a58ec4328268fd7b852b96545c9241
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 17:40:13 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,38 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+    minimo = 0
+    maximo = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            if val > maximo:
+                maximo = val
+            elif val < minimo:
+                minimo = val
+        else:
+
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
+
+            curkey = key
+            total = val
+            minimo = val
+            maximo = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
