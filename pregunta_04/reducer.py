0364f969020fad91186370a4776b749d8e5dffec
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-18 23:02:48 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys       
+
+    l = []  
+    for separar in sys.stdin:
+        separar = separar.strip().split()
+        l.append(line)
+    for i in sorted(set([i[0] for i in l]),key=lambda x: x[0]):
+        sys.stdout.write('{},{}\n'.format(i,l.count([i])))
