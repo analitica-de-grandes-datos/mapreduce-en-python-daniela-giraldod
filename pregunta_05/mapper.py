9d6737b5d2d7bd8c9004c69b5d77258e28bd176a
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 16:58:58 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -6,7 +6,8 @@
 import sys
 
 if __name__ == "__main__":
-for fec in sys.stdin:
-  fecha = fec.strip().split("  ")[1]
-  month =   fecha.strip().split('-')[1]
-  sys.stdout.write("{}\t1\n".format(month))
+    for fec in sys.stdin:
+      fecha = fec.strip().split("  ")[1]
+      month = fecha.strip().split('-')[1]
+      sys.stdout.write("{}\t1\n".format(month))
+
