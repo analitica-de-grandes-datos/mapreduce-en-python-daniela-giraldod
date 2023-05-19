4efbcfb6b8865bd6381c04c7c1db7f5e44abf7ab
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-19 16:28:47 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+    #map
+
+import sys
+
+for fec in sys.stdin:
+  fecha = fec.split("  ")
+  month =   fecha.split('-')[1]
+  sys.stdout.write("{}\t1\n".format(month))
