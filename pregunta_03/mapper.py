4eeccc5b339615865cd0b4e22137dd0e644b551d
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-18 22:47:28 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+#map
+for fil in sys.stdin:
+  separada = fil.split(",")
+  sys.stdout.write(separada[0]+","+separada[1])
