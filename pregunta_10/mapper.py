e1b2e1b2ef32f3fabd5c74e6cee7c449ca9e3534
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-23 23:29:19 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+      clave, val = line.split("\t")
+      val = list(val.strip().split(",")
+                 
+      for letra in val:
+            
+         sys.stdout.write("{}\t{}\n".format(letra, clave))
