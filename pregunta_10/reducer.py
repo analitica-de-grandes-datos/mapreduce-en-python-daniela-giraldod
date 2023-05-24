79787fa8080765d7253f10919d1faf06bb7a5b92
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-23 23:52:21 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -3,24 +3,28 @@
 #
 import sys
 
-curkey = None
-total = 0
+if __name__ == '__main__':
 
-for line in sys.stdin:
-    key, val = line.split("\t")
-    val = val.strip()
+    curkey = None
+    total = 0
 
-    if key == curkey:
-       nums = nums + "," + str(int(val))
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = val.strip()
+
+        if key == curkey:
+
+          nums = nums + "," + str(int(val))
           
-    else:
-        if curkey is not None:
+        else:
+            if curkey is not None:
 
-         sys.stdout.write("{}\t{}\n".format(curkey, nums))
+             sys.stdout.write("{}\t{}\n".format(curkey, nums))
 
-        curkey = key
-        nums = str(int(val))
+            curkey = key
+            nums = str(int(val))
             
 
-sys.stdout.write("{}\t{}\n".format(curkey, nums))
+    sys.stdout.write("{}\t{}\n".format(curkey, nums))
 
