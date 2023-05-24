e0d68791dff5e2f819767e0e69958e56423533a1
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-23 22:37:19 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+#map
+for linea in sys.stdin:
+  base = linea.strip().split("  ")
+  sys.stdout.write("{}\t{}\t{}\n".format(base[0], base[1] ,base[2]))
