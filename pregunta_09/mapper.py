d5990731a1592b7245e486ce2e26cb4d1f64de7c
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-23 23:05:19 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -5,7 +5,7 @@ import sys
 
 #map
 for linea in sys.stdin:
-  letra = linea.split("  ")[0]
-  fecha = linea.split("  ")[1]
-  num = int(linea.split("  ")[2])
+  letra = linea.split('   ')[0]
+  fecha = linea.split('   ')[1]
+  num = int(linea.split('   ')[2])
   sys.stdout.write("{}\t{}\t{}\n".format(letra,fecha,num))
