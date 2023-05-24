28cf253132a0aafc54f1dd37e90a3259be0045eb
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2023-05-23 22:51:56 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+  #armar las listas  
+lista=[]  
+
+for line in sys.stdin:
+   key, fecha, valor = line.split("\t") 
+   valor=int(valor) 
+   lista.append([key, fecha, valor]) 
+
+#ordenar
+lista = sorted(lista, key=lambda x: (x[2]))  
+lista= lista[0:6] 
+for linea in lista: 
+   sys.stdout.write("{}{}{}\n".format(linea[0], linea[1], linea[2]))
